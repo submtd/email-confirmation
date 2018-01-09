ce40435e7673be9cba4cd4350f4a2c0f856605a4
Steve Harmeyer <harmeyersteve@gmail.com>
2018-01-09 10:57:48 -0700

    initial commit

---
+++ src/Database/Migrations/2018_01_01_000001_add_confirmation_fields_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConfirmationFieldsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('confirmed')->default(false)->after('remember_token')->index();
+            $table->string('confirmation_code', 32)->nullable()->after('confirmed')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('confirmed');
+            $table->dropColumn('confirmation_code');
+        });
+    }
+}
