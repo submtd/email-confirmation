15a9e57fb89060aa9f98f40e43c606a287e92b47
Steve Harmeyer <harmeyersteve@gmail.com>
2018-01-10 15:25:56 -0700

    updates

--- src/Config/email-confirmation.php
+++ src/Config/email-confirmation.php
@@ -7,6 +7,7 @@ return [
     'redirectOnResend' => '/login',
     'redirectOnAlreadyConfirmed' => '/',
     'statusMessages' => [
+        'confirm' => 'You must confirm your email address before logging in. An email has been sent to {{ $email }}. If you have not received the confirmation email, we can <a href="{{ url(\'/confirm/\' . $user_id . \'/resend\') }}">resend it</a>.',
         'invalidUserId' => 'Invalid user id.',
         'alreadyConfirmed' => 'Your email address has already been confirmed.',
         'invalidToken' => 'Invalid confirmation token.',
